16bed3f2fb62b70422f8a621465651075c5b7cf2
Maxwell J. Koo <mjkoo90@gmail.com>
2013-04-06 20:30:53 -0400

    Fix covers directory creation, move functionality out of Event class. Fixes #1

--- event.py
+++ event.py
@@ -14,65 +14,64 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
+import contextlib
 import dbus
 import hashlib
+import os
 import sys
-import urllib2
 
-from os import path
+from urllib2 import urlopen, URLError
 
-class Event:
+def handle_event(type, **kwargs):
     """
-    Basic pianobar event handler to interpret event_command output to actions
-    useful to the rest of the python wrapper, including notification messages.
+    Read event parameters from stdin and handle events appropriately.
     """
 
-    def __init__(self):
-        """
-        Initialize the pianobar event handler and dbus notify
-        """
+    # Error from pianobar, disregard
+    if kwargs.get("pRet") != "1":
+        return
+
+    # Handle specific events
+    if type == "songstart":
+        title = kwargs.get("title")
+        cover_url = kwargs.get("coverArt")
+        artist_album = "by %s on %s" % (kwargs.get("artist"), kwargs.get("album"))
+
+        config_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+        filename = os.path.join(config_dir, "covers", hashlib.sha1(cover_url).hexdigest())
+        cover = fetch_cover(cover_url, filename)
 
         obj_path = "/org/freedesktop/Notifications"
         bus_name = iface_name = "org.freedesktop.Notifications"
         bus = dbus.SessionBus()
         obj = bus.get_object(bus_name, obj_path)
+        iface = dbus.Interface(obj, iface_name)
 
-        self.iface = dbus.Interface(obj, iface_name)
-        self.timeout = 5000
+        iface.Notify("", 0, cover, title, artist_album, [], [], 5000)
 
-    def execute(self, type, params={}):
-        """
-        Read event parameters from stdin and handle events appropriately.
-        """
+def fetch_cover(url, filename):
+    """
+    Fetches album art from the URL specified by pianobar, and saves to disk.
+    """
 
-        # Error from pianobar, disregard
-        if params["pRet"] != "1":
-            return
-
-        # Handle specific events
-        if type == "songstart":
-            title = params["title"]
-            artist_album = "by %s on %s" % (params["artist"], params["album"])
-            filename = "~/.config/pianobar/covers/%s.jpg" % hashlib.sha1(params["coverArt"]).hexdigest()
-            cover = self.fetch_cover(params["coverArt"], filename)
+    # If the "covers" directory does not exist under the pianobar config
+    # directory, create it
+    if not os.path.isdir(os.path.dirname(filename)):
+        os.mkdir(os.path.dirname(filename))
     
-            self.iface.Notify("", 0, cover, title, artist_album, [], [], self.timeout)
+    if not os.path.exists(filename):
+        try:
+            with contextlib.closing(urlopen(url)) as inf, open(filename, "wb") as outf:
+                outf.write(inf.read())
+        except URLError:
+            return ""
 
-    def fetch_cover(self, url, filename="~/.config/pianobar/album.jpg"):
-        """
-        Fetches album art from the URL specified by pianobar, and saves to disk.
-        """
+    return "file://%s" % filename
 
-        filename = path.abspath(path.expanduser(filename))
-        
-        if not path.exists(filename):
-            with open(filename, "wb") as f:
-                f.write(urllib2.urlopen(url).read())
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
 
-        return "file://%s" % filename
-
-
-if __name__ == "__main__":
     # Read event type from command arguments
     if len(sys.argv) < 2:
         print "error reading event type from command arguments"
@@ -86,5 +85,9 @@ if __name__ == "__main__":
         params[param.strip()] = value.strip()
 
     # Call the event handler
-    Event().execute(type, params)
+    handle_event(type, **params)
 
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
